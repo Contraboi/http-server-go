62a4e0f7f5629aacd0bda039a84c544f224d4a17
Selmir Nedzibi <Contraboi@users.noreply.github.com>
2024-05-25 01:41:51 +0200

    its too late

--- app/server.go
+++ app/server.go
@@ -10,10 +10,6 @@ func main() {
 		fmt.Println("Hello from /")
 		res.Send(200, "<h1>Hello</h1>")
 	})
-
-	myhttp.Get("/:fruit", func(res *myhttp.Response, ctx *myhttp.Context) {
-		res.Send(200, ctx.Params["fruit"])
-	})
 	myhttp.Get("/echo/:slug", func(res *myhttp.Response, ctx *myhttp.Context) {
 		fmt.Println("Hello from /echo/:slug")
 		res.WriteHeader("Content-Type", "text/plain")
