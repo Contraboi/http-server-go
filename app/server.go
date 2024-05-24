ac7ebe6b55a1053721eb62af31e9e437fbaad4f4
Selmir Nedzibi <Contraboi@users.noreply.github.com>
2024-05-24 21:59:38 +0200

    feat(server): parse path

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -16,9 +17,21 @@ func main() {
 	}
 
 	conn, err := l.Accept()
+	defer conn.Close()
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
+	path := strings.Split(string(buf), " ")[1]
+
+	if path != "/" {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		return
+
 	}
 
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
